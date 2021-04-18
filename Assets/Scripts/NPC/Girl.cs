543210725e365ba850660fe2cc1dd8e28613be01
Alam Rodriguez <alamgrodriguez@gmail.com>
2021-04-18 09:15:54 -0500

    Girl animation
    [Feature] Added girl Sprites and animations (Idle, walk).
    [Feature] Added the logic for the girl to play the animations based on the x axis velocity.

--- Assets/Scripts/NPC/Girl.cs
+++ Assets/Scripts/NPC/Girl.cs
@@ -14,6 +14,8 @@ public class Girl : MonoBehaviour
    // public Transform posMax;
 
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator anim;
 
     public float force;
     public float distancia;
@@ -25,17 +27,19 @@ public class Girl : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player"); //tomamos el player
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
         
     }
 
     void Update()
     {
         //if (Vector3.Distance(target.position, objectToMove.position) > 0.5f) //distancia entre el objeto y player
-
-            if(Vector3.Distance(transform.position, player.transform.position) < distancia)
-            {
-                Follow();
-            }
+        ManageAnimation();
+        if(Vector3.Distance(transform.position, player.transform.position) < distancia)
+        {
+            Follow();
+        }
     }
 
     public void Follow()
@@ -43,10 +47,17 @@ public class Girl : MonoBehaviour
         if(transform.position.x < player.transform.position.x)//Detecta la derecha
         {
             rb.velocity = new Vector2(force, rb.velocity.y); //derecha
+            spriteRenderer.flipX = true;
         }
         else 
         {
             rb.velocity = new Vector2(-force, rb.velocity.y); //Izquierda
+            spriteRenderer.flipX = false;
         }
+    }
+
+    public void ManageAnimation()
+    {
+        anim.SetFloat("VelX", Mathf.Abs(rb.velocity.x)); //Mandar al animador la velocidad horizontal (X) actual
     }
 }
